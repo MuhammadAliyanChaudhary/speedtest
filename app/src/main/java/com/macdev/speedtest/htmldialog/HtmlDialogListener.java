49dbe059bcfac6997101bf5ccaefd4f846f72550
MuhammadAliyanChaudhary <aliyanch03055@gmail.com>
2022-09-04 20:59:57 +0500

    First commit

---
+++ app/src/main/java/com/macdev/speedtest/htmldialog/HtmlDialogListener.java
@@ -0,0 +1,11 @@
+package com.macdev.speedtest.htmldialog;
+
+public abstract class HtmlDialogListener
+{
+
+    public abstract void onNegativeButtonPressed();
+
+    public abstract void onPositiveButtonPressed();
+
+    public void onDialogCancel() {};
+}
